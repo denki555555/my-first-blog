bd368ae35e1a61eb1d8c52e08f6f09cdd5380100
mdh94 <m.d.h940810@gmail.com>
2024-02-03 21:53:32 +0900

    Changed the html for the site

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+    # Djangoがビューを見つけるとき、http://127.0.0.1:8000にアクセスがあったら、
+    # views.post_list にリダイレクトさせるための設定
+    path('',views.post_list,name='post_list'),
+]
