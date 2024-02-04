765cf33211ea41e42f271a412a2b80f87c3b78c9
mdh94 <m.d.h940810@gmail.com>
2024-02-04 14:44:21 +0900

    detail edit avairable

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     # Djangoがビューを見つけるとき、http://127.0.0.1:8000にアクセスがあったら、
     # views.post_list にリダイレクトさせるための設定
     path('',views.post_list,name='post_list'),
-    path('post/<int:pk>/',views.post_detail,name='post_detail')
+    path('post/<int:pk>/',views.post_detail,name='post_detail'),
+    path('post/new/',views.post_new,name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
