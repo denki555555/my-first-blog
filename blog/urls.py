dbb2f736e8c0604c9b35de46bbe6c8252a3aadac
mdh94 <m.d.h940810@gmail.com>
2024-02-04 11:40:25 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     # Djangoがビューを見つけるとき、http://127.0.0.1:8000にアクセスがあったら、
     # views.post_list にリダイレクトさせるための設定
     path('',views.post_list,name='post_list'),
+    path('post/<int:pk>/',views.post_detail,name='post_detail')
 ]
